5527757c1abccc03c77aaab6a2f0495c1b12135e
brankbao(包健) <brankbao@webank.com>
2020-03-31 10:03:41 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e -x 
+sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
 apk -U add bash git  gcc musl-dev
 cd $(dirname $0)/..
 source $(dirname $0)/version.sh
