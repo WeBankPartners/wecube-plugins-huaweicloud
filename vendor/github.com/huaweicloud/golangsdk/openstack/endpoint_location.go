08e05d30e52db2934059e49bd896e65e517bf5df
tylertang <tylertang06@163.com>
2020-04-21 15:26:32 +0800

    remove fmt

--- vendor/github.com/huaweicloud/golangsdk/openstack/endpoint_location.go
+++ vendor/github.com/huaweicloud/golangsdk/openstack/endpoint_location.go
@@ -1,7 +1,6 @@
 package openstack
 
 import (
-        "fmt"
 	"github.com/huaweicloud/golangsdk"
 	tokens2 "github.com/huaweicloud/golangsdk/openstack/identity/v2/tokens"
 	tokens3 "github.com/huaweicloud/golangsdk/openstack/identity/v3/tokens"
@@ -73,7 +72,7 @@ func V3EndpointURL(catalog *tokens3.ServiceCatalog, opts golangsdk.EndpointOpts)
 	// Extract Endpoints from the catalog entries that match the requested Type, Interface,
 	// Name if provided, and Region if provided.
 
-	fmt.Printf("opts=%++v\n",opts)
+	// fmt.Printf("opts=%++v\n", opts)
 	var endpoints = make([]tokens3.Endpoint, 0, 1)
 	for _, entry := range catalog.Entries {
 		if (entry.Type == opts.Type) && (opts.Name == "" || entry.Name == opts.Name) {
@@ -102,7 +101,7 @@ func V3EndpointURL(catalog *tokens3.ServiceCatalog, opts golangsdk.EndpointOpts)
 
 	// Extract the URL from the matching Endpoint.
 	for _, endpoint := range endpoints {
-		fmt.Printf("%++v\n",endpoints)
+		// fmt.Printf("%++v\n", endpoints)
 		return golangsdk.NormalizeURL(endpoint.URL), nil
 	}
 
